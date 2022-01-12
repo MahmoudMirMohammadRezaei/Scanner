5d689b82b2363067f37e6578e208c193c2ff133e
Sohrabi <Sohrabi@DESKTOP-KR101UB>
2022-01-12 11:21:50 +0330

    رفع خطای while بی نهایت

--- SelfHostWcfService/WIAScanner.cs
+++ SelfHostWcfService/WIAScanner.cs
@@ -143,31 +143,31 @@ namespace SelfHost
                         image = null;
                         // add file to output list
                         images.Add(Image.FromFile(fileName));
-
-                        //item = null;
-                        //determine if there are any more pages waiting
-                        WIA.Property documentHandlingSelect = null;
-                        WIA.Property documentHandlingStatus = null;
-                        foreach (WIA.Property prop in device.Properties)
-                        {
-                            if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_SELECT)
-                                documentHandlingSelect = prop;
-                            if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_STATUS)
-                                documentHandlingStatus = prop;
-                        }
-                        // assume there are no more pages
-                        hasMorePages = false;
-                        // may not exist on flatbed scanner but required for feeder
-                        if (documentHandlingSelect != null)
-                        {
-                            // check for document feeder
-                            if ((Convert.ToUInt32(documentHandlingSelect.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_SELECT.FEEDER) != 0)
-                            {
-                                hasMorePages = ((Convert.ToUInt32(documentHandlingStatus.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_STATUS.FEED_READY) != 0);
-                            }
-                        }
                     }
 
+
+                    //item = null;
+                    //determine if there are any more pages waiting
+                    WIA.Property documentHandlingSelect = null;
+                    WIA.Property documentHandlingStatus = null;
+                    foreach (WIA.Property prop in device.Properties)
+                    {
+                        if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_SELECT)
+                            documentHandlingSelect = prop;
+                        if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_STATUS)
+                            documentHandlingStatus = prop;
+                    }
+                    // assume there are no more pages
+                    hasMorePages = false;
+                    // may not exist on flatbed scanner but required for feeder
+                    if (documentHandlingSelect != null)
+                    {
+                        // check for document feeder
+                        if ((Convert.ToUInt32(documentHandlingSelect.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_SELECT.FEEDER) != 0)
+                        {
+                            hasMorePages = ((Convert.ToUInt32(documentHandlingStatus.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_STATUS.FEED_READY) != 0);
+                        }
+                    }
                 }
                 catch (Exception exc)
                 {
