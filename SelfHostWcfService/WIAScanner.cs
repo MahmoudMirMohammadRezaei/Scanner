16f612dde65f28f8466a9d5d45589688367c85dd
Mahmood <mahmodmreza@gmail.com>
2022-01-12 11:01:03 +0330

    test values

--- SelfHostWcfService/WIAScanner.cs
+++ SelfHostWcfService/WIAScanner.cs
@@ -37,20 +37,20 @@ namespace SelfHost
         /// Use scanner to scan an image (with user selecting the scanner from a dialog).
         /// </summary>
         /// <returns>Scanned images.</returns>
-        public static List<Image> Scan(string logPath)
-        {
-            WIA.ICommonDialog dialog = new WIA.CommonDialog();
-            WIA.Device device = dialog.ShowSelectDevice(WIA.WiaDeviceType.UnspecifiedDeviceType, true, false);
-            if (device != null)
-            {
-                writeToLog(logPath, "line 88 - device.DeviceID: " + device.DeviceID);
-                return Scan(device.DeviceID, logPath);
-            }
-            else
-            {
-                throw new Exception("You must select a device for scanning.");
-            }
-        }
+        //public static List<Image> Scan(string logPath)
+        //{
+        //    WIA.ICommonDialog dialog = new WIA.CommonDialog();
+        //    WIA.Device device = dialog.ShowSelectDevice(WIA.WiaDeviceType.UnspecifiedDeviceType, true, false);
+        //    if (device != null)
+        //    {
+        //        writeToLog(logPath, "line 88 - device.DeviceID: " + device.DeviceID);
+        //        return Scan(device.DeviceID, logPath);
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("You must select a device for scanning.");
+        //    }
+        //}
 
 
         public static void writeToLog(string logPath, string s)
@@ -76,97 +76,62 @@ namespace SelfHost
         {
             List<Image> images = new List<Image>();
             bool hasMorePages = true;
+
+            WIA.ICommonDialog dialog = new WIA.CommonDialog();
+            WIA.Device device = dialog.ShowSelectDevice(WIA.WiaDeviceType.ScannerDeviceType);
+            WIA.Items items = dialog.ShowSelectItems(device);
+
+
             while (hasMorePages)
             {
-                // select the correct scanner using the provided scannerId parameter
-                WIA.DeviceManager manager = new WIA.DeviceManager();
-                WIA.Device device = null;
-                foreach (WIA.DeviceInfo info in manager.DeviceInfos)
-                {
-                    if (info.DeviceID == scannerId)
-                    {
-                        writeToLog(logPath, "line 88 - scannerId: " + scannerId);
-                        // connect to scanner
-                        device = info.Connect();
-                        break;
-                    }
-                }
-                // device was not found
-                if (device == null)
-                {
-                    writeToLog(logPath, "line 97: device is null");
-                    // enumerate available devices
-                    string availableDevices = "";
-                    foreach (WIA.DeviceInfo info in manager.DeviceInfos)
-                    {
-                        writeToLog(logPath, "line 102 - info.DeviceID: " + info.DeviceID);
-                        availableDevices += info.DeviceID + "\n";
-                    }
 
-                    // show error with available devices
-                    throw new Exception("The device with provided ID could not be found. Available Devices:\n" + availableDevices);
-                }
+
 
                 try
                 {
-                    WIA.Item item = null;
-                    foreach (WIA.Item item2 in device.Items)
+                    foreach (WIA.Item item in items)
                     {
-                        item = item2;
-                        break;
-                    }
-
-                    // scan image
-                    //WIA.ICommonDialog wiaCommonDialog = new WIA.CommonDialog();
-                    //WIA.ImageFile image = (WIA.ImageFile)wiaCommonDialog.ShowTransfer(item, wiaFormatBMP, false);
-
-                    //SetDeviceIntProperty(ref device, 1048, 1);
-                    WIA.ICommonDialog wiaCommonDialog = new WIA.CommonDialog();
-                    //var tempRes = wiaCommonDialog.ShowItemProperties();
-                    Thread.Sleep(2000);
-                    var tempRes = wiaCommonDialog.ShowTransfer(item, wiaFormatBMP, true);
-                    Thread.Sleep(2000);
-                    WIA.ImageFile image = null;
-                    if (tempRes != null)
-                    {
-                        image = (WIA.ImageFile)tempRes;
-                    }
-
-
-                    if (image != null)
-                    {
-                        // save to temp file
-                        string fileName = Path.GetTempFileName();
-                        writeToLog(logPath, "line 136 - fileName: " + fileName);
-                        File.Delete(fileName);
-                        image.SaveFile(fileName);
-                        image = null;
-                        // add file to output list
-                        images.Add(Image.FromFile(fileName));
-
-                        //item = null;
-                        //determine if there are any more pages waiting
-                        WIA.Property documentHandlingSelect = null;
-                        WIA.Property documentHandlingStatus = null;
-                        foreach (WIA.Property prop in device.Properties)
+                        WIA.ImageFile image = null;
+                        dialog = new WIA.CommonDialog();
+                        image = (WIA.ImageFile)dialog.ShowTransfer(item, "{B96B3CAB-0728-11D3-9D7B-0000F81EF32E}", false);
+                        if (image != null && image.FileData != null)
                         {
-                            if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_SELECT)
-                                documentHandlingSelect = prop;
-                            if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_STATUS)
-                                documentHandlingStatus = prop;
-                        }
-                        // assume there are no more pages
-                        hasMorePages = false;
-                        // may not exist on flatbed scanner but required for feeder
-                        if (documentHandlingSelect != null)
-                        {
-                            // check for document feeder
-                            if ((Convert.ToUInt32(documentHandlingSelect.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_SELECT.FEEDER) != 0)
+                            string fileName = Path.GetTempFileName();
+
+
+                            writeToLog(logPath, "line 136 - fileName: " + fileName);
+                            File.Delete(fileName);
+                            image.SaveFile(fileName);
+                            image = null;
+                            // add file to output list
+                            images.Add(Image.FromFile(fileName));
+
+                            //item = null;
+                            //determine if there are any more pages waiting
+                            WIA.Property documentHandlingSelect = null;
+                            WIA.Property documentHandlingStatus = null;
+                            foreach (WIA.Property prop in device.Properties)
                             {
-                                hasMorePages = ((Convert.ToUInt32(documentHandlingStatus.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_STATUS.FEED_READY) != 0);
+                                if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_SELECT)
+                                    documentHandlingSelect = prop;
+                                if (prop.PropertyID == WIA_PROPERTIES.WIA_DPS_DOCUMENT_HANDLING_STATUS)
+                                    documentHandlingStatus = prop;
+                            }
+                            // assume there are no more pages
+                            hasMorePages = false;
+                            // may not exist on flatbed scanner but required for feeder
+                            if (documentHandlingSelect != null)
+                            {
+                                // check for document feeder
+                                if ((Convert.ToUInt32(documentHandlingSelect.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_SELECT.FEEDER) != 0)
+                                {
+                                    hasMorePages = ((Convert.ToUInt32(documentHandlingStatus.get_Value()) & WIA_DPS_DOCUMENT_HANDLING_STATUS.FEED_READY) != 0);
+                                }
                             }
                         }
                     }
+
+
 
                 }
                 catch (Exception exc)
